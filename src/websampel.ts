9a679221e3271da0337bc2c2097a4325b136f809
hosein.rajaie <hosein.rajaie@vas24.ir>
2021-03-13 09:32:16 +0330

    add catch

--- src/websampel.ts
+++ src/websampel.ts
@@ -1,10 +1,12 @@
-import  * as janusvideo from "janus-video-room-js"
+import * as janusvideo from "janus-video-room-js"
 
 declare global {
   interface Window { janusvideo: any; }
 }
-window.janusvideo =janusvideo;
-let sampelRoom = new janusvideo.Janus.JanusRoom("wss://janus.conf.meetecho.com/ws", { keepalive: 'true' });
+window.janusvideo = janusvideo;
+var server = "wss://janus.conf.meetecho.com/ws";
+//server  = "ws://webrtc.10d.ir:8188";
+let sampelRoom = new janusvideo.Janus.JanusRoom(server, { keepalive: 'true' });
 /**
  * on joined room success
  */
@@ -20,7 +22,7 @@ sampelRoom.eventNeedVideoRemote.on((event: any) => {
     const video = document.createElement('video');
     video.id = event.feedInfo.id;
     video.autoplay = true;
-    if(document.getElementById(`h${event.feedInfo.id}`)){ document.getElementById(`h${event.feedInfo.id}`).remove() }
+    if (document.getElementById(`h${event.feedInfo.id}`)) { document.getElementById(`h${event.feedInfo.id}`).remove() }
     document.getElementById('remotvideocontainer').innerHTML += (`<h2 id="h${event.feedInfo.id}"> ${event.feedInfo.id} display : ${event.feedInfo.display} </h2>`)
     document.getElementById('remotvideocontainer').appendChild(video);
     event.element = video;
@@ -30,7 +32,7 @@ sampelRoom.eventNeedVideoRemote.on((event: any) => {
   }
   let spanID = "no_video_" + event.feedInfo.id.toString();
   let elementSpan = document.getElementById(spanID);
-  if(elementSpan){ elementSpan.remove()}
+  if (elementSpan) { elementSpan.remove() }
 })
 /**
  *  On Leaving feed from room
@@ -40,10 +42,10 @@ sampelRoom.eventOnLeaving.on((feedID: number) => {
   let spanID = "no_video_" + feedID.toString();
   let elementSpan = document.getElementById(spanID);
   let h2 = document.getElementById(`h${feedID}`);
-  if (elementVideo) { elementVideo.remove();}
-  if(elementSpan){ elementSpan.remove()}
-  if(h2){ h2.remove()}
- })
+  if (elementVideo) { elementVideo.remove(); }
+  if (elementSpan) { elementSpan.remove() }
+  if (h2) { h2.remove() }
+})
 /**
  * on Unpublished  video and  audio 
  */
@@ -58,11 +60,28 @@ sampelRoom.eventOnUnpublished.on((feedID: number) => {
     elementVideo.parentNode.replaceChild(elementSpan, elementVideo);
     //document.getElementById(feedID.toString()).remove();
   }
- })
-
+})
+/*
 sampelRoom.joinRoom(1234, 'fullname2').then((data: any) => {
   console.log('joinRoom 1234');
 })
+*/
+//room id : 5175
+//password: 35239205 
+sampelRoom.joinRoom(3278, 'fullname2',
+  {
+    "ptype": "publisher"
+
+  }
+).then((data: any) => {
+  console.log('joinRoom 1234');
+}).catch((e) => {
+  alert(e.message);
+}
+
+);
+
+
 
 
 
